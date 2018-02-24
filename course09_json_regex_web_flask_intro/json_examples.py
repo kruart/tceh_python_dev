c99f72566638df658a10fff84aea970c60f643f1
we0z <weoz@ukr.net>
2018-02-24 12:07:28 +0200

    course09: json

---
+++ course09_json_regex_web_flask_intro/json_examples.py
@@ -0,0 +1,19 @@
+import json
+
+from course09_json_regex_web_flask_intro.files import way_better
+
+if __name__ == '__main__':
+    data = way_better('data.json')
+    print('raw data is', data, type(data))
+    print()
+
+    # From string to object:
+    obj = json.loads(data)
+    print(obj, type(obj))
+
+    print(obj['object'], obj['boolean'])
+
+    # From object to string
+    print('dumping object to text:')
+    obj['new-value'] = 'secret'
+    print(json.dumps(obj, sort_keys=True, indent=4))
