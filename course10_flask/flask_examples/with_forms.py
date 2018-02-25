cc5b4728f1c3d164b51327e96952939b30d5c326
we0z <weoz@ukr.net>
2018-02-25 14:00:58 +0200

    course10: classwork(validators)

--- course10_flask/flask_examples/with_forms.py
+++ course10_flask/flask_examples/with_forms.py
@@ -1,8 +1,9 @@
+import datetime
 from flask import Flask, request
 
 # pip install flask-WTF
 from flask_wtf import FlaskForm
-from wtforms import StringField, validators
+from wtforms import StringField, validators, DateField
 
 
 class ContactForm(FlaskForm):
@@ -12,6 +13,25 @@ class ContactForm(FlaskForm):
     email = StringField(label='E-mail', validators=[
         validators.Length(min=6, max=35), validators.Email()
     ])
+
+    # class work: сделать валидатор для job:
+    # 1. job должны быть только такие: IT, Bank, HR
+    # 2. job поле должно быть обязательнным
+    job = StringField(label='Job', validators=[
+        validators.DataRequired(),
+        validators.AnyOf(values=['IT', 'Bank', 'HR'])
+    ])
+    # валидация дня рождения пользователя
+    # 1. валидируем месяц, если он равен текущему - все хорошо, иначе - плохо
+    birthday = DateField(label='Birthday')  # '1988-05-02' format
+
+    def validate(self):
+        rv = FlaskForm.validate(self)
+
+        if self.birthday.data.month == datetime.date.today().month and rv:
+            return True
+        else:
+            return False
 
 
 app = Flask(__name__)
