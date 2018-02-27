d7f63166b99ccd613783cf34cd30cb6252048fda
we0z <weoz@ukr.net>
2018-02-27 13:01:30 +0200

    course11: flask config

---
+++ course11_context_manager_flask_config_request/request_example/outside_request.py
@@ -0,0 +1,28 @@
+from flask import Flask, request
+import os
+
+app = Flask(__name__)
+
+# to update multiple keys at once you can use the dict.update() method:
+app.config.update(
+    DEBUG=True,
+    # SECRET_KEY=os.environ['S'],
+    WTF_CSRF_ENABLED=False,
+)
+# app.config['DEBUG'] = True
+# app.config['SECRET_KEY'] = os.environ['S']
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    x = 5 / 0
+    return 'hello world!', 200
+
+
+# print(request, type(request), request.method)
+with app.test_request_context('/'):
+    print(request, type(request), request.method)
+
+
+if __name__ == '__main__':
+    app.run()
